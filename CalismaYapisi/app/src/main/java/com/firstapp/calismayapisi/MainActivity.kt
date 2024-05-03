1f43b3568af90b162e8498c3fb0c337416aa5284
Gorur56 <sevim.gorur01@gmail.com>
2024-05-04 00:23:02 +0300

    Kotlin ViewBinding ile MainActivity 'de nesneleri bağlamak

--- CalismaYapisi/app/src/main/java/com/firstapp/calismayapisi/MainActivity.kt
+++ CalismaYapisi/app/src/main/java/com/firstapp/calismayapisi/MainActivity.kt
@@ -5,16 +5,26 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.firstapp.calismayapisi.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater) //layoutInflater: Görsel tasarımları aktarmamızı sağlar.
+        setContentView(binding.root) //TAsarımı bağlamış oluyoruz.
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        binding.buttonDetay.setOnClickListener {
+            Snackbar.make(it, "Merhaba", Snackbar.LENGTH_SHORT).show()
+            //Butona basınca anasayfa NAsılsın olcak
+            binding.textView.text = "Nasılsın"
         }
     }
 }
