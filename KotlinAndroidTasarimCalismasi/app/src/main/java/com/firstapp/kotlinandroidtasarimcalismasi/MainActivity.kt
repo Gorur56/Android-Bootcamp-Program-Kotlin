dc82a4fe8acf479ed483d8e589d6c751deabb29d
Gorur56 <sevim.gorur01@gmail.com>
2024-05-03 14:05:36 +0300

    Kotlin Android Pizza ekranı tasarımı

--- KotlinAndroidTasarimCalismasi/app/src/main/java/com/firstapp/kotlinandroidtasarimcalismasi/MainActivity.kt
+++ KotlinAndroidTasarimCalismasi/app/src/main/java/com/firstapp/kotlinandroidtasarimcalismasi/MainActivity.kt
@@ -16,5 +16,10 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        //Yazılım kısmında R harfi res klasörünü temsil eder.
+        //Xml kısmında @ işareti res klasörünü temsil eder. Manifeast dosyası gibi.
+        //Left(Android) - Start(android) - Leading (ios) //Bunlar Görsel nesnein sol kısmınız simgeler.
+        //Right - End (Android) - Trailing (ios) //Bunlar Görsel nesnein sağ kısmınız simgeler.
     }
 }
