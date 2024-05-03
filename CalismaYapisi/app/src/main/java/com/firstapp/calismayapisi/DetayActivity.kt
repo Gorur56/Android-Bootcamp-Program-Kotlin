e3338e2a9fdac208030c78ea0940b5481b365b52
Gorur56 <sevim.gorur01@gmail.com>
2024-05-04 00:29:06 +0300

    Kotlin Detay activity 'i viewBinding 'e bağlama

--- CalismaYapisi/app/src/main/java/com/firstapp/calismayapisi/DetayActivity.kt
+++ CalismaYapisi/app/src/main/java/com/firstapp/calismayapisi/DetayActivity.kt
@@ -5,16 +5,22 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.firstapp.calismayapisi.databinding.ActivityDetayBinding
 
 class DetayActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityDetayBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_detay)
+
+        binding = ActivityDetayBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        binding.textViewBilgi.text = "Merhaba"
     }
 }
