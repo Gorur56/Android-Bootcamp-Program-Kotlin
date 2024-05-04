4098af2ee44332655eeb3133309b3bbf82e39279
Gorur56 <sevim.gorur01@gmail.com>
2024-05-04 10:18:47 +0300

    Kotlin Navigation Component kullanımı (Fragments)

---
+++ NavigationComponentKullanimi/app/src/main/java/com/firstapp/navigationcomponentkullanimi/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.firstapp.navigationcomponentkullanimi
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    //Fragment 'ları bir activity üzerinde oluşturuyoruz. Activity olmadan Fragment oluşturamayız.
+    //Fragment 'alr sayfa geçişini daha görsel ve pratik hale getirir.
+    //Geçişlerde veri transferi ve animasyon ekleyebiliriz.
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+    }
+}
