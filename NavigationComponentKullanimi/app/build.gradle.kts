0461f4400ae63b3a637636e832077ad1c8ee3afd
Gorur56 <sevim.gorur01@gmail.com>
2024-05-04 20:27:42 +0300

    viewBinding kurulum

--- NavigationComponentKullanimi/app/build.gradle.kts
+++ NavigationComponentKullanimi/app/build.gradle.kts
@@ -7,6 +7,10 @@ android {
     namespace = "com.firstapp.navigationcomponentkullanimi"
     compileSdk = 34
 
+    buildFeatures{
+        viewBinding = true
+    }
+
     defaultConfig {
         applicationId = "com.firstapp.navigationcomponentkullanimi"
         minSdk = 19
--- NavigationComponentKullanimi/app/src/main/java/com/firstapp/navigationcomponentkullanimi/AnasayfaFragment.kt
+++ NavigationComponentKullanimi/app/src/main/java/com/firstapp/navigationcomponentkullanimi/AnasayfaFragment.kt
@@ -5,8 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.firstapp.navigationcomponentkullanimi.databinding.FragmentAnasayfaBinding
 
 class AnasayfaFragment : Fragment() {
+    private lateinit var binding:FragmentAnasayfaBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_anasayfa, container, false)
