edbf9b02b47c900bb1e43d9e5e2acb802a7ce82d
Gorur56 <sevim.gorur01@gmail.com>
2024-05-05 19:20:30 +0300

    image settings in bottomsheetfragment

--- NavigationComponentKullanimi/app/src/main/java/com/firstapp/navigationcomponentkullanimi/BottomSheetFragment.kt
+++ NavigationComponentKullanimi/app/src/main/java/com/firstapp/navigationcomponentkullanimi/BottomSheetFragment.kt
@@ -5,14 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.firstapp.navigationcomponentkullanimi.databinding.FragmentBottomSheetBinding
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.google.android.material.snackbar.Snackbar
 
 class BottomSheetFragment : BottomSheetDialogFragment() { //Fragment yerine BottomSheetDialogFragment() 'den miras alıyoruz.
     private lateinit var binding: FragmentBottomSheetBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = FragmentBottomSheetBinding.inflate(inflater, container, false)
+
+        binding.imageViewPaylas.setOnClickListener {
+            Snackbar.make(it,"Paylaşıldı",Snackbar.LENGTH_SHORT).show()
+        }
+
+        binding.imageViewKopyala.setOnClickListener {
+            Snackbar.make(it,"Kopyalandı",Snackbar.LENGTH_SHORT).show()
+        }
         return binding.root
     }
 }
