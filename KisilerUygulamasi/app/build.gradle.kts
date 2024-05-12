3c9bd182823e1406accc241af8805cac4bdcb07c
Gorur56 <sevim.gorur01@gmail.com>
2024-05-12 19:50:08 +0300

    Kotlin Kişiler Uygulaması create fragment and view binding

--- KisilerUygulamasi/app/build.gradle.kts
+++ KisilerUygulamasi/app/build.gradle.kts
@@ -7,6 +7,10 @@ android {
     namespace = "com.firstapp.kisileruygulamasi"
     compileSdk = 34
 
+    buildFeatures{
+        viewBinding = true
+    }
+
     defaultConfig {
         applicationId = "com.firstapp.kisileruygulamasi"
         minSdk = 19
