a41948bcd8f2d1d44447756231e227eee76b7cdc
Gorur56 <sevim.gorur01@gmail.com>
2024-05-12 21:29:03 +0300

    Kotlin Kişiler Uygulaması fab button gecis

--- KisilerUygulamasi/app/src/main/java/com/firstapp/kisileruygulamasi/ui/fragment/AnasayfaFragment.kt
+++ KisilerUygulamasi/app/src/main/java/com/firstapp/kisileruygulamasi/ui/fragment/AnasayfaFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import com.firstapp.kisileruygulamasi.R
 import com.firstapp.kisileruygulamasi.databinding.ActivityMainBinding
 import com.firstapp.kisileruygulamasi.databinding.FragmentAnasayfaBinding
@@ -15,6 +16,12 @@ class AnasayfaFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
         binding = FragmentAnasayfaBinding.inflate(inflater,container,false)
+
+        binding.fab.setOnClickListener{
+            //fab 'a basınca kisiKayırGEcis sayfasına geçecektir.
+            Navigation.findNavController(it).navigate(R.id.kisiKayitGecis) // it == binding.fab
+        }
+
         return binding.root
     }
 }
