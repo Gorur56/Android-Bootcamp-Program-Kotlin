2f6fbab3e43c2824c04d5d2c4b067ed12c170a64
Gorur56 <sevim.gorur01@gmail.com>
2024-05-12 23:15:12 +0300

    Kotlin Kişiler uygulaması AnaSayfaFragment add SearchView query code

--- KisilerUygulamasi/app/src/main/java/com/firstapp/kisileruygulamasi/ui/fragment/AnasayfaFragment.kt
+++ KisilerUygulamasi/app/src/main/java/com/firstapp/kisileruygulamasi/ui/fragment/AnasayfaFragment.kt
@@ -1,10 +1,13 @@
 package com.firstapp.kisileruygulamasi.ui.fragment
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SearchView
+import androidx.appcompat.widget.SearchView.OnQueryTextListener
 import androidx.navigation.Navigation
 import com.firstapp.kisileruygulamasi.R
 import com.firstapp.kisileruygulamasi.data.entity.Kisiler
@@ -34,6 +37,26 @@ class AnasayfaFragment : Fragment() {
             Navigation.findNavController(it).navigate(gecis)
         }*/
 
+        binding.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
+            override fun onQueryTextChange(newText: String): Boolean {
+                //Harf girdikçe veya sildikçe arama yapar
+                ara(newText)
+                return true
+            }
+
+            override fun onQueryTextSubmit(query: String): Boolean {
+                //Arama ikonuna basınca bize sonuç getirir.
+                ara(query)
+                return true
+            }
+
+        })
+
         return binding.root
+    }
+
+    fun ara(arananKelime:String)
+    {
+        Log.e("Aranan Kelime: ","$arananKelime")
     }
 }
