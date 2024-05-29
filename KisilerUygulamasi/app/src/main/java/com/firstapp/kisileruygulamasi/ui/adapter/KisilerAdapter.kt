14e2045a5374c12b6baff020ad63f128aef60989
Gorur56 <sevim.gorur01@gmail.com>
2024-05-29 09:33:06 +0300

    Eklediğimiz cancel ikonu ile basınca kişiyi silme işlemi yaptık. Bunu LOG üzerinden test ettik.

--- KisilerUygulamasi/app/src/main/java/com/firstapp/kisileruygulamasi/ui/adapter/KisilerAdapter.kt
+++ KisilerUygulamasi/app/src/main/java/com/firstapp/kisileruygulamasi/ui/adapter/KisilerAdapter.kt
@@ -1,6 +1,7 @@
 package com.firstapp.kisileruygulamasi.ui.adapter
 
 import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,6 +12,7 @@ import com.firstapp.kisileruygulamasi.databinding.CardTasarimBinding
 import com.firstapp.kisileruygulamasi.databinding.FragmentAnasayfaBinding
 import com.firstapp.kisileruygulamasi.ui.fragment.AnasayfaFragment
 import com.firstapp.kisileruygulamasi.ui.fragment.AnasayfaFragmentDirections
+import com.google.android.material.snackbar.Snackbar
 
 class KisilerAdapter(var mContext: Context, var kisilerListesi:List<Kisiler>)
     : RecyclerView.Adapter<KisilerAdapter.CardTasarimTutucu>(){
@@ -41,10 +43,23 @@ class KisilerAdapter(var mContext: Context, var kisilerListesi:List<Kisiler>)
             val gecis = AnasayfaFragmentDirections.kisiDEtayGecis( kisi = kisi)
             Navigation.findNavController(it).navigate(gecis)
         }
+
+        t.imageViewSil.setOnClickListener {
+            Snackbar.make(it,"${kisi.kisi_ad} silinsin mi?",Snackbar.LENGTH_SHORT)
+                .setAction("EVET"){
+                    sil(kisi.kisi_id)
+                }
+                .show()
+        }
     }
 
     override fun getItemCount(): Int {
         //listede kaç tane item var.
         return kisilerListesi.size
+    }
+
+    fun sil(kisi_id:Int)
+    {
+        Log.e("Kisi sil",kisi_id.toString())
     }
 }
