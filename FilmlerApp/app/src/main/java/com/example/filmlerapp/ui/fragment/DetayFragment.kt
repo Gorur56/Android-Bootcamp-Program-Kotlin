5ff57d9908686cd0315372951b3274195cdb99a9
Gorur56 <sevim.gorur01@gmail.com>
2024-05-31 16:56:27 +0300

    Android kotlin Filmler Uygulaması

---
+++ FilmlerApp/app/src/main/java/com/example/filmlerapp/ui/fragment/DetayFragment.kt
@@ -0,0 +1,18 @@
+package com.example.filmlerapp.ui.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.filmlerapp.R
+import com.example.filmlerapp.databinding.FragmentAnasayfaBinding
+import com.example.filmlerapp.databinding.FragmentDetayBinding
+
+class DetayFragment : Fragment() {
+    private lateinit var binding: FragmentDetayBinding
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = FragmentDetayBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+}
