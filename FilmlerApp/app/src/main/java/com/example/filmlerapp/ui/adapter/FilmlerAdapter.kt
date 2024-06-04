47937a45824983f705813d63677215962fbc6b0c
Gorur56 <sevim.gorur01@gmail.com>
2024-06-04 11:19:00 +0300

    onCreateViewHolder viewBinding kurulum

--- FilmlerApp/app/src/main/java/com/example/filmlerapp/ui/adapter/FilmlerAdapter.kt
+++ FilmlerApp/app/src/main/java/com/example/filmlerapp/ui/adapter/FilmlerAdapter.kt
@@ -1,21 +1,22 @@
 package com.example.filmlerapp.ui.adapter
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.filmlerapp.data.entity.Filmler
 import com.example.filmlerapp.databinding.CardTasarimBinding
+import com.example.filmlerapp.databinding.FragmentAnasayfaBinding
 
 class FilmlerAdapter( var mContext:Context, var filmlerListesi:List<Filmler>)
     : RecyclerView.Adapter<FilmlerAdapter.CardTasarimTutucu>(){
     //inner class card üzerindeki görsel nesneler erişmek için kullanılır.
     inner class CardTasarimTutucu(var tasarim:CardTasarimBinding) : RecyclerView.ViewHolder(tasarim.root)
-    {
-
-    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CardTasarimTutucu {
-        TODO("Not yet implemented")
+        //Burada ViewBinding yapıyoruz.
+        val binding = CardTasarimBinding.inflate(LayoutInflater.from(mContext),parent, false)
+        return CardTasarimTutucu(binding)
     }
 
     override fun onBindViewHolder(holder: CardTasarimTutucu, position: Int) {
