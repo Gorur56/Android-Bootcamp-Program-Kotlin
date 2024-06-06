f88c6e5e7121ba6b612bd606c4ab532760691e70
Gorur56 <sevim.gorur01@gmail.com>
2024-06-06 12:12:02 +0300

    finish filmlerApp and create MVVM kullanimi

--- FilmlerApp/app/src/main/java/com/example/filmlerapp/ui/fragment/DetayFragment.kt
+++ FilmlerApp/app/src/main/java/com/example/filmlerapp/ui/fragment/DetayFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.navArgs
 import com.example.filmlerapp.R
 import com.example.filmlerapp.databinding.FragmentAnasayfaBinding
 import com.example.filmlerapp.databinding.FragmentDetayBinding
@@ -13,6 +14,15 @@ class DetayFragment : Fragment() {
     private lateinit var binding: FragmentDetayBinding
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = FragmentDetayBinding.inflate(inflater,container,false)
+
+        val bundle:DetayFragmentArgs by navArgs()
+        val film = bundle.film
+
+        binding.toolbarDetay.title = film.ad
+        binding.ivFilm.setImageResource(
+            resources.getIdentifier(film.resim,"drawable",requireContext().packageName))
+
+        binding.tvFiyat.text = "${film.fiyat} $"
         return binding.root
     }
 }
