c80a46df5c77088b08a581fe46bf36f7d0dc3620
Gorur56 <sevim.gorur01@gmail.com>
2024-06-06 14:29:24 +0300

    MVVM DataBinding activity settings

--- MVVMKullanimi/app/src/main/java/com/example/mvvmkullanimi/MainActivity.kt
+++ MVVMKullanimi/app/src/main/java/com/example/mvvmkullanimi/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
 import com.example.mvvmkullanimi.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,14 +14,19 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        //binding = ActivityMainBinding.inflate(layoutInflater)
+
+        //DataBinding
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
+        //Diğer kısınlar ViewBinding ile aynıdır.
         binding.textVieSonuc.text = "0"
 
         binding.buttonTopla.setOnClickListener {
