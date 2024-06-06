fe36b2ae459ce8b10787dab506b25a2f80e4388a
Gorur56 <sevim.gorur01@gmail.com>
2024-06-06 13:55:40 +0300

    MVVM dizayn, binding, toplama butonu

--- MVVMKullanimi/app/src/main/java/com/example/mvvmkullanimi/MainActivity.kt
+++ MVVMKullanimi/app/src/main/java/com/example/mvvmkullanimi/MainActivity.kt
@@ -1,20 +1,38 @@
 package com.example.mvvmkullanimi
 
 import android.os.Bundle
+import android.view.inputmethod.InputBinding
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.mvvmkullanimi.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        binding.textVieSonuc.text = "0"
+
+        binding.buttonTopla.setOnClickListener {
+            val alinanSayi1 = binding.editTextTextSayi1.text.toString()
+            val alinanSayi2 = binding.editTextTextSAyi2.text.toString()
+
+            val sayi1 = alinanSayi1.toInt()
+            val sayi2 = alinanSayi2.toInt()
+
+            val toplam = sayi1 + sayi2
+
+            binding.textVieSonuc.text = toplam.toString()
         }
     }
 }
