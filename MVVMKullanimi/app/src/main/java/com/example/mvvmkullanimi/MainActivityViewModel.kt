32298582bd1bfaa86baae653cac387963ba56d44
Gorur56 <sevim.gorur01@gmail.com>
2024-06-07 10:12:18 +0300

    MVVM repository 'i ViewModel 'e baglama

--- MVVMKullanimi/app/src/main/java/com/example/mvvmkullanimi/MainActivityViewModel.kt
+++ MVVMKullanimi/app/src/main/java/com/example/mvvmkullanimi/MainActivityViewModel.kt
@@ -2,6 +2,9 @@ package com.example.mvvmkullanimi
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivityViewModel : ViewModel() {
     //Class'ta yapılacak işlemler View Model 'e yptırılır.
@@ -9,20 +12,30 @@ class MainActivityViewModel : ViewModel() {
 
     //LiveData
     var sonuc = MutableLiveData("0")
+    var mrepo =MatematikRepository()
     fun toplamaYap(alinanSayi1:String, alinanSayi2: String){
-        val sayi1 = alinanSayi1.toInt()
+       /*Bu işlemi artık DataSource yapıyor.
+       val sayi1 = alinanSayi1.toInt()
         val sayi2 = alinanSayi2.toInt()
-
         val toplam = sayi1 + sayi2
-        sonuc.value = toplam.toString()
+        sonuc.value = toplam.toString()*/
+        //Dispatchers.Main => arayüz işlemlerinde kullanılır.
+        CoroutineScope(Dispatchers.Main).launch {
+            //LiveData 'ya sonuç ekleyeceğiz
+            sonuc.value = mrepo.toplamaYap(alinanSayi1,alinanSayi2)
+        }
     }
 
     fun carpmaYap(alinanSayi1:String, alinanSayi2: String){
 
+        /*Bu işlemi artık DataSource yapıyor.
         val sayi1 = alinanSayi1.toInt()
         val sayi2 = alinanSayi2.toInt()
 
         val carpma = sayi1 * sayi2
-        sonuc.value = carpma.toString()
+        sonuc.value = carpma.toString()*/
+        CoroutineScope(Dispatchers).launch {
+            sonuc.value = mrepo.carpmaYap(alinanSayi1,alinanSayi2)
+        }
     }
 }
