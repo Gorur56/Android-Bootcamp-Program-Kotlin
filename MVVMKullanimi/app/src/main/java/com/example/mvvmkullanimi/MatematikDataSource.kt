5acd3f6d341254d99ec7b031842586cb006b8caf
Gorur56 <sevim.gorur01@gmail.com>
2024-06-07 09:46:01 +0300

    MVVM Data Source Corourtine (Asenkron) ile veri gönderme

---
+++ MVVMKullanimi/app/src/main/java/com/example/mvvmkullanimi/MatematikDataSource.kt
@@ -0,0 +1,29 @@
+package com.example.mvvmkullanimi
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+class MatematikDataSource {
+    //Asenkron bir çalışma ile veri gönderiyoruz.
+    suspend fun toplamaYap(alinanSayi1:String, alinanSayi2: String) : String =
+        withContext(Dispatchers.IO){
+            //Coroutine ile veri gönderme
+            //return edeceksek withContenxt kullanılır.
+            //Dispatchers.IO: Veri tabanı ile işlem yapılacaksa kullanılır.
+            val sayi1 = alinanSayi1.toString()
+            val sayi2 = alinanSayi2.toString()
+            val toplam = sayi1 + sayi2
+
+            return@withContext toplam.toString()
+        }
+
+
+    suspend fun carpmaYap(alinanSayi1:String, alinanSayi2: String) : String =
+        withContext(Dispatchers.IO){
+            val sayi1 = alinanSayi1.toString()
+            val sayi2 = alinanSayi2.toString()
+            val toplam = sayi1 + sayi2
+
+            return@withContext toplam.toString()
+        }
+}
