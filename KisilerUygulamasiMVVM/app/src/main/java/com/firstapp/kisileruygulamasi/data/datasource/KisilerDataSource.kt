d190cda691af16e3f2373cd05ff77da1edddf0d5
Gorur56 <sevim.gorur01@gmail.com>
2024-06-07 16:13:46 +0300

    create Datasource, KisilerRepository and KisiKayitViewModel

--- KisilerUygulamasiMVVM/app/src/main/java/com/firstapp/kisileruygulamasi/data/datasource/KisilerDataSource.kt
+++ KisilerUygulamasiMVVM/app/src/main/java/com/firstapp/kisileruygulamasi/data/datasource/KisilerDataSource.kt
@@ -1,4 +1,10 @@
 package com.firstapp.kisileruygulamasi.data.datasource
 
+import android.util.Log
+
 class KisilerDataSource {
+    suspend fun kaydet(kisi_ad:String, kisi_tel:String)
+    {
+        Log.e("Kisi Kaydet","$kisi_ad - $kisi_tel")
+    }
 }
--- KisilerUygulamasiMVVM/app/src/main/java/com/firstapp/kisileruygulamasi/data/repo/KisilerRepository.kt
+++ KisilerUygulamasiMVVM/app/src/main/java/com/firstapp/kisileruygulamasi/data/repo/KisilerRepository.kt
@@ -1,4 +1,8 @@
 package com.firstapp.kisileruygulamasi.data.repo
 
+import com.firstapp.kisileruygulamasi.data.datasource.KisilerDataSource
+
 class KisilerRepository {
+    var kds = KisilerDataSource()
+    suspend fun  kaydet(kisi_ad:String, kisi_tel:String) = kds.kaydet(kisi_ad,kisi_tel)
 }
