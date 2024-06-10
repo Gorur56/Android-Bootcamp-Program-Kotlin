2a8e18ae74dc33aa87f4a2d053c1328b68146f09
Gorur56 <sevim.gorur01@gmail.com>
2024-06-10 12:47:22 +0300

    Filmler App create LiveData and init function

--- FilmlerAppMVVM/app/src/main/java/com/example/filmlerapp/ui/viewmodel/AnasayfaViewModel.kt
+++ FilmlerAppMVVM/app/src/main/java/com/example/filmlerapp/ui/viewmodel/AnasayfaViewModel.kt
@@ -1,6 +1,8 @@
 package com.example.filmlerapp.ui.viewmodel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.filmlerapp.data.entity.Filmler
 import com.example.filmlerapp.data.repo.FilmlerRepository
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -9,9 +11,17 @@ import kotlinx.coroutines.launch
 class AnasayfaViewModel : ViewModel() {
     var frepo = FilmlerRepository()
 
+    //Veri aldığımız için LiveData kullanmamız gerekiyor.
+    val filmlerListesi = MutableLiveData<List<Filmler>>()
+
+    init {
+        //programbaşlayınca listeyi yükler.
+        filmleriYukle()
+    }
+
     fun filmleriYukle(){
         CoroutineScope(Dispatchers.Main).launch {
-            frepo.filmleriYukle()
+            filmlerListesi.value = frepo.filmleriYukle()
         }
     }
 }
