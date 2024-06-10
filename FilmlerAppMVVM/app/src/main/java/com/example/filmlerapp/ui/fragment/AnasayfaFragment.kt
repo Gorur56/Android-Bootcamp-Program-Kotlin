3dadef0600ce88946b2395c6f98a5680c48d0dcc
Gorur56 <sevim.gorur01@gmail.com>
2024-06-10 11:45:42 +0300

    Filmler App Create DataSource

--- FilmlerAppMVVM/app/src/main/java/com/example/filmlerapp/ui/fragment/AnasayfaFragment.kt
+++ FilmlerAppMVVM/app/src/main/java/com/example/filmlerapp/ui/fragment/AnasayfaFragment.kt
@@ -23,22 +23,6 @@ class AnasayfaFragment : Fragment() {
         //Aşağıdaki kodu layout 'a taşıdık
         //binding.filmRv.layoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
 
-        val filmlerListesi = ArrayList<Filmler>()
-
-        val f1 = Filmler(1,"Djongo", "django",24)
-        val f2 = Filmler(2,"Interstellar", "interstellar",32)
-        val f3 = Filmler(1,"Inception", "inception",16)
-        val f4 = Filmler(1,"The Hateful Eight", "thehatefuleight",28)
-        val f5 = Filmler(1,"The Pianist", "thepianist",24)
-        val f6 = Filmler(1,"Anadoluda", "anadoluda",24)
-
-        filmlerListesi.add(f1)
-        filmlerListesi.add(f2)
-        filmlerListesi.add(f3)
-        filmlerListesi.add(f4)
-        filmlerListesi.add(f5)
-        filmlerListesi.add(f6)
-
         val filmlerAdapter = FilmlerAdapter(requireContext(),filmlerListesi)
 
         // binding.filmRv.adapter = filmlerAdapter layout dosyasına taşındıç
