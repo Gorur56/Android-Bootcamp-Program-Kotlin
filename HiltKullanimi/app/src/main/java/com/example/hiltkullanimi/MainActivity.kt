760557c3e209d059d8bae60017b22dae697afc9f
Gorur56 <sevim.gorur01@gmail.com>
2024-06-10 15:55:23 +0300

    HILT hilt 'i main activity 'e ekleme

--- HiltKullanimi/app/src/main/java/com/example/hiltkullanimi/MainActivity.kt
+++ HiltKullanimi/app/src/main/java/com/example/hiltkullanimi/MainActivity.kt
@@ -5,11 +5,16 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint //Baslangıç noktası
 class MainActivity : AppCompatActivity() {
     //HILT KULLANIMI: Kod tekrarın önlemek için kullanılır.
     //Hilt private değişkenleri kullanamıyor.
+    @Inject //Hilt
     lateinit var kargo: Kargo
+    @Inject
     lateinit var internet: Internet
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,10 +27,10 @@ class MainActivity : AppCompatActivity() {
         }
 
         //Internet ve KArgo Adrese bağımlıdır. Hilt bu bağımlılığı kolaylaştırır.
-        kargo = Kargo()
+        //kargo = Kargo() //Hilt 'en dolayı nesne oluşturmaya gerek yok
         kargo.gonder()
 
-        internet = Internet()
+        //internet = Internet()
         internet.basvuruYap()
     }
 }
