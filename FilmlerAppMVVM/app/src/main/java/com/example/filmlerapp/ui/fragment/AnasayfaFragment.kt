d813667d0e370b305d9c694e33c3dd3f4104557b
Gorur56 <sevim.gorur01@gmail.com>
2024-06-10 10:51:45 +0300

    Film App Toolbar ve Recycleview 'i adapter 'i Fragment sayfasina baglama

--- FilmlerAppMVVM/app/src/main/java/com/example/filmlerapp/ui/fragment/AnasayfaFragment.kt
+++ FilmlerAppMVVM/app/src/main/java/com/example/filmlerapp/ui/fragment/AnasayfaFragment.kt
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.example.filmlerapp.R
 import com.example.filmlerapp.data.entity.Filmler
 import com.example.filmlerapp.databinding.FragmentAnasayfaBinding
 import com.example.filmlerapp.ui.adapter.FilmlerAdapter
@@ -13,11 +15,11 @@ import com.example.filmlerapp.ui.adapter.FilmlerAdapter
 class AnasayfaFragment : Fragment() {
     private lateinit var binding: FragmentAnasayfaBinding
     override fun onCreateView( inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        binding = FragmentAnasayfaBinding.inflate(inflater,container,false)
+        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_anasayfa, container,false)
+        binding.anasayfaToolbarBaslik = "Filmler"
 
-        binding.toolbarAnasayfa.title = "Filmler"
-
-        binding.filmRv.layoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
+        //Aşağıdaki kodu layout 'a taşıdık
+        //binding.filmRv.layoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
 
         val filmlerListesi = ArrayList<Filmler>()
 
@@ -36,7 +38,9 @@ class AnasayfaFragment : Fragment() {
         filmlerListesi.add(f6)
 
         val filmlerAdapter = FilmlerAdapter(requireContext(),filmlerListesi)
-        binding.filmRv.adapter = filmlerAdapter
+
+        // binding.filmRv.adapter = filmlerAdapter layout dosyasına taşındıç
+        binding.filmlerAdapter = filmlerAdapter
         return binding.root
     }
 }
