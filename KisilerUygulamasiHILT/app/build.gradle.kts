538e22ca1f697a45aba062a9778de1e54691111c
Gorur56 <sevim.gorur01@gmail.com>
2024-06-10 16:59:05 +0300

    HILt library kurulumu

--- KisilerUygulamasiHILT/app/build.gradle.kts
+++ KisilerUygulamasiHILT/app/build.gradle.kts
@@ -1,7 +1,10 @@
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.jetbrains.kotlin.android)
-    id("androidx.navigation.safeargs.kotlin")
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    kotlin("kapt")
+    id("com.google.dagger.hilt.android")
+    id("dagger.hilt.android.plugin")
+    id("kotlinx-serialization")
 }
 
 android {
@@ -15,7 +18,7 @@ android {
 
     defaultConfig {
         applicationId = "com.firstapp.kisileruygulamasi"
-        minSdk = 19
+        minSdk = 27
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
@@ -50,10 +53,22 @@ dependencies {
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
+    implementation("com.google.dagger:hilt-android-gradle-plugin:2.51")
+    implementation("com.google.android.material:material:1.12.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 
     implementation("androidx.lifecycle:lifecycle-viewmodel:2.8.1")
     implementation("androidx.activity:activity-ktx:1.9.0")
+
+    implementation("com.google.dagger:hilt-android:2.51")
+    kapt("com.google.dagger:hilt-compiler:2.51")
+    kapt("androidx.hilt:hilt-compiler:1.2.0")
+    implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
+    implementation("androidx.compose.material:material:1.6.7")
+}
+
+kapt {
+    correctErrorTypes = true
 }
