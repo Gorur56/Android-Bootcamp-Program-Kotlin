d4c3542147ec9d075c842584ed798b9bcbfa0472
Gorur56 <sevim.gorur01@gmail.com>
2024-06-10 15:36:00 +0300

    HILT Kullanici yani tuketici kismine HILT Ekleme

--- HiltKullanimi/app/src/main/java/com/example/hiltkullanimi/Internet.kt
+++ HiltKullanimi/app/src/main/java/com/example/hiltkullanimi/Internet.kt
@@ -1,9 +1,11 @@
 package com.example.hiltkullanimi
 
+import android.location.Address
 import android.util.Log
+import javax.inject.Inject
 
-class Internet {
-    val adres = Adres("Kadıköy / Istanbul")
+class Internet @Inject constructor( var adres: Adres) {
+    //val adres = Adres("Kadıköy / Istanbul") //Hilt ile kullanacağız.
 
     fun basvuruYap(){
         Log.e("Sonuc","Internet bavurusu ${adres.adresBilgisi} adresine yapıldı")
--- HiltKullanimi/app/src/main/java/com/example/hiltkullanimi/Kargo.kt
+++ HiltKullanimi/app/src/main/java/com/example/hiltkullanimi/Kargo.kt
@@ -1,9 +1,10 @@
 package com.example.hiltkullanimi
 
 import android.util.Log
+import javax.inject.Inject
 
-class Kargo {
-    val adres = Adres("Kadıköy / Istanbul")
+class Kargo @Inject constructor( var adres: Adres){
+    //val adres = Adres("Kadıköy / Istanbul") //Hilt ile alacağız.
 
     fun gonder(){
         Log.e("Sonuc","Kargo ${adres.adresBilgisi} adresine gönderildi.")
