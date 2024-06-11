b24e2fe08453d69f3a3194c65d967612990d370c
Gorur56 <sevim.gorur01@gmail.com>
2024-06-11 15:07:25 +0300

    DataStore MainActivity ad olusturma ve okunma

--- PreferencesDataStorekullanimi/app/src/main/java/com/example/preferencesdatastorekullanimi/MainActivity.kt
+++ PreferencesDataStorekullanimi/app/src/main/java/com/example/preferencesdatastorekullanimi/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.example.preferencesdatastorekullanimi
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.isActive
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +20,17 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val ap = AppPref(this)
+
+        CoroutineScope(Dispatchers.Main).launch {
+            //Kayıt işlemleri
+            ap.kayitAd("Ahmet")
+
+            //okuma İşlemleri
+            val gelenAd = ap.okuAd()
+            Log.e("Gelen Ad",gelenAd)
         }
     }
 }
