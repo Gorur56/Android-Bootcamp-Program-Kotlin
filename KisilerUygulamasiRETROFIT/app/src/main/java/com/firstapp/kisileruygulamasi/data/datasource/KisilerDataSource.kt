f7b2f4bc921a5163579f707a9ae97ec5784ec257
Gorur56 <sevim.gorur01@gmail.com>
2024-06-12 21:54:58 +0300

    Retrofit create

--- KisilerUygulamasiRETROFIT/app/src/main/java/com/firstapp/kisileruygulamasi/data/datasource/KisilerDataSource.kt
+++ KisilerUygulamasiRETROFIT/app/src/main/java/com/firstapp/kisileruygulamasi/data/datasource/KisilerDataSource.kt
@@ -2,14 +2,12 @@ package com.firstapp.kisileruygulamasi.data.datasource
 
 import android.util.Log
 import com.firstapp.kisileruygulamasi.data.entity.Kisiler
-import com.firstapp.kisileruygulamasi.room.KisilerDao
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
-class KisilerDataSource(val kdao:KisilerDao) {
+class KisilerDataSource {
     suspend fun kisiYukle() : List<Kisiler> =
         withContext(Dispatchers.IO){
-            /*Artık veritabanından değerleri çekiyoruz
             //İtem oluşturma için arrayList oluşturuyoruz.
             val kisilerListesi = ArrayList<Kisiler>()
 
@@ -21,46 +19,32 @@ class KisilerDataSource(val kdao:KisilerDao) {
             //Nesneleri arrayliste ekle
             kisilerListesi.add(k1)
             kisilerListesi.add(k2)
-            kisilerListesi.add(k3)*/
+            kisilerListesi.add(k3)
 
-            return@withContext kdao.kisileriYukle()
+            return@withContext kisilerListesi
         }
 
     suspend fun ara(aramaKelimesi:String) : List<Kisiler> =
         withContext(Dispatchers.IO) {
-            /*val kisilerlistesi = ArrayList<Kisiler>()
+            val kisilerlistesi = ArrayList<Kisiler>()
 
             val k1 = Kisiler(1, "Ahmet", "523634896")
-            kisilerlistesi.add(k1)*/
+            kisilerlistesi.add(k1)
 
-            return@withContext kdao.ara(aramaKelimesi)
+            return@withContext kisilerlistesi
         }
     suspend fun kaydet(kisi_ad:String, kisi_tel:String)
     {
-
         Log.e("Kisi Kaydet","$kisi_ad - $kisi_tel")
-
-        //İnterface ile kayıt
-        //id otomatik olduğu için 0 giriyoruz.
-        val yeniKisi = Kisiler(0,kisi_ad, kisi_tel)
-        kdao.kaydet(yeniKisi)
     }
 
     suspend fun guncelle(kisi_id:Int, kisi_ad: String, kisi_tel: String)
     {
         Log.e("Kişi Kaydet", "$kisi_id, $kisi_ad, $kisi_tel")
-
-        //İnterface ile update
-        val guncellenenKisi = Kisiler(kisi_id,kisi_ad,kisi_tel)
-        kdao.guncelle(guncellenenKisi)
     }
 
     suspend fun sil(kisi_id: Int)
     {
         Log.e("Kişi Sil", "$kisi_id")
-
-        //İnterface ile sil
-        val silinenKisi = Kisiler(kisi_id,"", "")
-        kdao.sil(silinenKisi)
     }
 }
