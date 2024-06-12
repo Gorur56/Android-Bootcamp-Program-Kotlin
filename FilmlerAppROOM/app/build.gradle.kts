540aa76e83f63da7745af9e27ca2dc9543962314
Gorur56 <sevim.gorur01@gmail.com>
2024-06-12 14:27:41 +0300

    Create Asset file and add filmler_app.sqlite database

--- FilmlerAppROOM/app/build.gradle.kts
+++ FilmlerAppROOM/app/build.gradle.kts
@@ -70,6 +70,11 @@ dependencies {
     kapt("androidx.hilt:hilt-compiler:1.2.0")
     implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
     implementation("androidx.compose.material:material:1.6.7")
+
+    implementation("androidx.room:room-runtime:2.5.0-beta02")
+    kapt("androidx.room:room-compiler:2.5.0-beta02")
+    implementation("androidx.room:room-ktx:2.6.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.4.3")
 }
 
 kapt {
