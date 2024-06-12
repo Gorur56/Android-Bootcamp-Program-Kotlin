07be6f1c1c3d7f87bf314d165909258c9b68fd99
Gorur56 <sevim.gorur01@gmail.com>
2024-06-12 09:38:52 +0300

    ROOM kutuphanesini ekleme

--- KisilerUygulamasiROOM/app/build.gradle.kts
+++ KisilerUygulamasiROOM/app/build.gradle.kts
@@ -71,6 +71,11 @@ dependencies {
     kapt("androidx.hilt:hilt-compiler:1.2.0")
     implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
     implementation("androidx.compose.material:material:1.6.7")
+
+    implementation("androidx.room:room-runtime:2.5.0-beta02")
+    kapt("androidx.room:room-compiler:2.5.0-beta02")
+    implementation("androidx.room:room-ktx:2.4.3")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.4.3")
 }
 
 kapt {
