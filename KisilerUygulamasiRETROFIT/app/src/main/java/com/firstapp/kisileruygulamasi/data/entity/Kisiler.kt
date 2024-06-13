9445140a84c211e9dc8067e1bf550ac912af7afc
Gorur56 <sevim.gorur01@gmail.com>
2024-06-13 10:13:33 +0300

    CRUD işlemler ve KisilerCevap için success için sınıf oluşturma

--- KisilerUygulamasiRETROFIT/app/src/main/java/com/firstapp/kisileruygulamasi/data/entity/Kisiler.kt
+++ KisilerUygulamasiRETROFIT/app/src/main/java/com/firstapp/kisileruygulamasi/data/entity/Kisiler.kt
@@ -5,4 +5,26 @@ import java.io.Serializable
 data class Kisiler( var kisi_id:Int,
                     var kisi_ad:String,
                     var kisi_tel:String) : Serializable {
+
+    /* JSON FORMATI
+    {"kisiler":[{"kisi_id":"15674","kisi_ad":"Ahmet","kisi_tel":"3333"},
+    {"kisi_id":"16169","kisi_ad":"x","kisi_tel":"y"}],"success":1}
+
+    VERİ ÇIKIŞI
+
+    {
+       "kisiler":[
+          {
+             "kisi_id":"15674",
+             "kisi_ad":"Ahmet",
+             "kisi_tel":"3333"
+          },
+          {
+             "kisi_id":"16169",
+             "kisi_ad":"x",
+             "kisi_tel":"y"
+          }
+       ],
+       "success":1
+    }*/
 }
---
+++ KisilerUygulamasiRETROFIT/app/src/main/java/com/firstapp/kisileruygulamasi/data/entity/KisilerCevap.kt
@@ -0,0 +1,5 @@
+package com.firstapp.kisileruygulamasi.data.entity
+
+data class KisilerCevap(var kisiler: List<Kisiler>,
+                        var success: Int ) {
+}
