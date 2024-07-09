71cc21a3988b75580ac4a1a71248de16383653ff
Gorur56 <sevim.gorur01@gmail.com>
2024-07-09 16:37:43 +0300

    Create tablayout struct

--- TabLayoutKullanimi/app/src/main/java/com/example/tablayoutkullanimi/MainActivity.kt
+++ TabLayoutKullanimi/app/src/main/java/com/example/tablayoutkullanimi/MainActivity.kt
@@ -5,8 +5,17 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
+
 
 class MainActivity : AppCompatActivity() {
+    private val fragmentList = ArrayList<Fragment>()
+    private val fragmentBaslikList = ArrayList<String>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +25,34 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        fragmentList.add(BirinciFragment())
+        fragmentList.add(IkinciFragment())
+        fragmentList.add(UcuncuFragment())
+
+        val adapter = MyViewPagerAdapter(this)
+
+        val viewPager: ViewPager2 = findViewById(R.id.viewPager)
+        viewPager.adapter = adapter
+
+        fragmentBaslikList.add("Bir")
+        fragmentBaslikList.add("İki")
+        fragmentBaslikList.add("Üç")
+
+        val tabLayout: TabLayout = findViewById(R.id.tablayout)
+        TabLayoutMediator(tabLayout, viewPager){tab, position ->
+            tab.setText(fragmentBaslikList[position])
+        }.attach()
+    }
+
+    inner class MyViewPagerAdapter(fragmentActivity: FragmentActivity): FragmentStateAdapter(fragmentActivity){
+        override fun getItemCount(): Int {
+            return fragmentList.size
+        }
+
+        override fun createFragment(position: Int): Fragment {
+            return fragmentList[position]
+        }
+
     }
 }
