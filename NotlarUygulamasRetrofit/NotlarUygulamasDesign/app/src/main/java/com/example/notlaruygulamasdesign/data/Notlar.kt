b5a41bfba056ff883351634ae89433a8f098918f
Gorur56 <sevim.gorur01@gmail.com>
2024-07-15 15:23:30 +0300

    Crate Notlar application with Retrofit library

---
+++ NotlarUygulamasRetrofit/NotlarUygulamasDesign/app/src/main/java/com/example/notlaruygulamasdesign/data/Notlar.kt
@@ -0,0 +1,9 @@
+package com.example.notlaruygulamasdesign.data
+
+import java.io.Serializable
+
+data class Notlar(var not_id: Int,
+                  var ders_adi: String,
+                  var not1: Int,
+                  var not2: Int) : Serializable {
+}
