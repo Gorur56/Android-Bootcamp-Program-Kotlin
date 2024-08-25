c8fae0ca37f64efc1a5ff3719b6a8515146805e6
Gorur56 <sevim.gorur01@gmail.com>
2024-08-25 16:54:06 +0300

    create firebase table and create kisi

--- FireBase Realtime Database Kullanimi/app/src/main/java/com/example/firebaserealtimedatabasekullanimi/MainActivity.kt
+++ FireBase Realtime Database Kullanimi/app/src/main/java/com/example/firebaserealtimedatabasekullanimi/MainActivity.kt
@@ -5,6 +5,9 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.Firebase
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.database
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +19,11 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val database = FirebaseDatabase.getInstance() //Firebase 'den yetki alma
+
+        val refKisiler = database.getReference("kisiler") //Tablo oluşturma
+        val kisi = Kisiler("Ahmet",18) //Değer ata
+        refKisiler.push().setValue(kisi) // Atanan değerin kaydını oluştur.
     }
 }
