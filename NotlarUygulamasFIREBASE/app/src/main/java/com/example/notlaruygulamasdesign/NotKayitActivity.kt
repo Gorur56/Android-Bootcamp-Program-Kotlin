1ff52714d980e6604ce2e3402ffb0d6ed2c15b78
Gorur56 <sevim.gorur01@gmail.com>
2024-09-12 15:54:47 +0300

    ders kayit firebase

--- NotlarUygulamasFIREBASE/app/src/main/java/com/example/notlaruygulamasdesign/NotKayitActivity.kt
+++ NotlarUygulamasFIREBASE/app/src/main/java/com/example/notlaruygulamasdesign/NotKayitActivity.kt
@@ -2,15 +2,21 @@ package com.example.notlaruygulamasdesign
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.TextUtils
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.notlaruygulamasdesign.data.Notlar
 import com.example.notlaruygulamasdesign.databinding.ActivityMainBinding
 import com.example.notlaruygulamasdesign.databinding.ActivityNotKayitBinding
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class NotKayitActivity : AppCompatActivity() {
     private lateinit var binding: ActivityNotKayitBinding
+    private lateinit var refNotlar: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,7 +34,36 @@ class NotKayitActivity : AppCompatActivity() {
         binding.notKayitActivityToolbarTitle = "Not Kayıt"
         setSupportActionBar(binding.toolbarNotKayit)
 
+        val db = FirebaseDatabase.getInstance()
+        refNotlar = db.getReference("notlar")
+
         binding.buttonKaydet.setOnClickListener {
+            //trim beyaz boşlukları alır
+            val ders_adi = binding.editTextDers.text.toString().trim()
+            val not1 = binding.editTextNot1.text.toString().trim()
+            val not2 = binding.editTextNot2.text.toString().trim()
+
+            //Ders adı, not1 veya not2 boşsa uyarı ver
+            if( TextUtils.isEmpty(ders_adi)) {
+                //uyarı ver kodu durdur
+                Snackbar.make(binding.toolbarNotKayit,"Ders adını giriniz.", Snackbar.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            if ( TextUtils.isEmpty(not1)){
+                Snackbar.make(binding.toolbarNotKayit, "1. notu giriniz.", Snackbar.LENGTH_SHORT ).show()
+                return@setOnClickListener
+            }
+
+            if ( TextUtils.isEmpty(not2)){
+                Snackbar.make(binding.toolbarNotKayit, "2. Notu giriniz.", Snackbar.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            //Yukarıdaki koşulalr sağlanıyorsa. Database 'e not kaydedebiliriz.
+            val not = Notlar("", ders_adi, not1.toInt(), not2.toInt())
+            refNotlar.push().setValue(not)
+
             startActivity(Intent(this@NotKayitActivity, MainActivity::class.java))
             finish()
         }
