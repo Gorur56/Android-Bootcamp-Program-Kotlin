fd03057bcd40f414495bf93d6aa894d92bebb1e8
Gorur56 <sevim.gorur01@gmail.com>
2024-09-18 11:59:03 +0300

    complete notlar uygulamasi

---
+++ SozlukUygulamasiFIREBASE/SozlukUygulamasi/app/src/main/java/com/example/sozlukuygulamasi/data/Kelimeler.kt
@@ -0,0 +1,16 @@
+package com.example.sozlukuygulamasi.data
+
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+import java.io.Serializable
+
+data class Kelimeler( @SerializedName("kelime_id")
+                      @Expose
+                      var id: Int,
+                      @SerializedName("ingilizce")
+                      @Expose
+                      var ingilizce: String,
+                      @SerializedName("turkce")
+                      @Expose
+                      var turkce: String ) : Serializable {
+}
