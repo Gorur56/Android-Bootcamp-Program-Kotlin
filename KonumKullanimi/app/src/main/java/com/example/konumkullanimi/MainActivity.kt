cf0544d310dbfcabfb12b84807b4aa4245ac684a
Gorur56 <sevim.gorur01@gmail.com>
2024-10-09 19:17:01 +0300

    add access fine location permition

--- KonumKullanimi/app/src/main/java/com/example/konumkullanimi/MainActivity.kt
+++ KonumKullanimi/app/src/main/java/com/example/konumkullanimi/MainActivity.kt
@@ -1,14 +1,20 @@
 package com.example.konumkullanimi
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.konumkullanimi.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private var izinKontrol = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -19,6 +25,34 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        binding.buttonKonumAl.setOnClickListener {
+            //Lokasyon izin onaylandı mı kontol et.
+            //Bunu manifest dosyasındaki     <uses-permission android:name="android.permission.ACCESS_FINE_LOCATION"/> yapıyor.
+
+            //Manifest = android olanı seç
+            izinKontrol = ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION)
+
+            if( izinKontrol == PackageManager.PERMISSION_GRANTED) { //İzin onaylanmışsa
+
+            }else {
+                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 100)
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        //Gelen request 'i buradan alacağız.
+        if( requestCode == 100 ) {
+            //grantResults arrayOf içindeki izinleri simgeler
+            if( grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                //Onaylandıysa toast mesajı ver
+                Toast.makeText(applicationContext,"İzin onaylandı.",Toast.LENGTH_SHORT).show()
+            }else {
+                Toast.makeText(applicationContext,"İzin onaylanmadı.",Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
