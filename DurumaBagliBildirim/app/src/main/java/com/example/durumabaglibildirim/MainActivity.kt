0581cbf2b76c2452f2fc9a406aba126e9d7bd4be
Gorur56 <sevim.gorur01@gmail.com>
2024-10-10 17:05:07 +0300

    notification olusturduk. oreo dan eski surum ve sonrası surum iciin ayri ayarlar yaptik. butona basinca bildirin geliyor. Bildirime basinca mainActivity 'e gecis yapiyor.

--- DurumaBagliBildirim/app/src/main/java/com/example/durumabaglibildirim/MainActivity.kt
+++ DurumaBagliBildirim/app/src/main/java/com/example/durumabaglibildirim/MainActivity.kt
@@ -1,20 +1,79 @@
 package com.example.durumabaglibildirim
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.durumabaglibildirim.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        binding.buttonBildir.setOnClickListener {
+            val builder: NotificationCompat.Builder
+
+            val bildirimYoneticisi = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+            val intent = Intent(this, MainActivity::class.java) //Bildirimin gösterileceği activity
+
+            val gidilecekIntent = PendingIntent.getActivity(this,1,intent,PendingIntent.FLAG_UPDATE_CURRENT)
+
+            //Genel tanımlamalar bitti şimdi bildirimi oluşturuyoruz.
+            if( Build.VERSION.SDK_INT >= Build.VERSION_CODES.O ) {
+                //Oreo ve üzeri sürüm ise;
+
+                val kanalId = "kanalId"
+                val kanalAd = "KanalAd"
+                val kanalTanitim = "KanalTanıtım"
+                val kanalOnceligi = NotificationManager.IMPORTANCE_HIGH
+
+                var kanal: NotificationChannel? = bildirimYoneticisi.getNotificationChannel(kanalId)
+
+                if( kanal == null) {
+                    kanal = NotificationChannel(kanalId,kanalAd,kanalOnceligi)
+                    kanal.description = kanalTanitim
+                    bildirimYoneticisi.createNotificationChannel(kanal)
+                }
+
+                builder = NotificationCompat.Builder(this, kanalId)
+                builder.setContentTitle("Başlık")
+                    .setContentText("İçerik")
+                    .setSmallIcon(R.drawable.resim)
+                    .setContentIntent(gidilecekIntent)
+                    .setAutoCancel(true) //Bildirime tıklayınca otomatik olarak kaybolur.
+            } else {
+                //Oreodan eski sürüm ise;
+                builder = NotificationCompat.Builder(this)
+                builder.setContentTitle("Başlık")
+                    .setContentText("İçerik")
+                    .setSmallIcon(R.drawable.resim)
+                    .setContentIntent(gidilecekIntent)
+                    .setAutoCancel(true) //Bildirime tıklayınca otomatik olarak kaybolur.
+                    .priority = Notification.PRIORITY_HIGH //Öncelik
+            }
+
+            //Bildirimi göster
+            bildirimYoneticisi.notify(1,builder.build())
         }
     }
 }
