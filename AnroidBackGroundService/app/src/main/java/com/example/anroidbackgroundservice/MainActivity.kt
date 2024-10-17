cdc6047c3b2ee03eb09ef8cdce4125dbd5b26c97
Gorur56 <sevim.gorur01@gmail.com>
2024-10-17 13:38:30 +0300

    complete musc player service

--- AnroidBackGroundService/app/src/main/java/com/example/anroidbackgroundservice/MainActivity.kt
+++ AnroidBackGroundService/app/src/main/java/com/example/anroidbackgroundservice/MainActivity.kt
@@ -1,20 +1,32 @@
 package com.example.anroidbackgroundservice
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.anroidbackgroundservice.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        binding.buttonStart.setOnClickListener {
+            startService(Intent(this, MusicService::class.java))
+        }
+
+        binding.buttonStop.setOnClickListener {
+            stopService(Intent(this, MusicService::class.java))
         }
     }
 }
